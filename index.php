cb51603b0b5b29bf712eeb042b698747615d4414
Student <std@aahn02.edu>
2020-12-01 16:46:04 +0700

    leetuyen

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Giới Thiệu Bản Thân</title>
+
+</head>
+<body>
+	<div>
+		
+	<h1 style="text-align: center;color: white;background-color: black;margin-bottom: 0px;">Giới thiệu chung</h1>
+	<div style="margin: 0px auto; width: 960px;">
+		<img src="C:\Users\std\Downloads\lee.jpg" style="float: left;width: 300px;height: 290px;padding: 0px 100px 0px 0px;">
+	</div>
+	<div style="padding: 0px 300px;">
+	<h3>Họ và Tên: Lê Sĩ Tuyển</h3>
+	<h3>Ngày Sinh: 06/12/2002</h3>
+	<h3>Quê Quán: Hà Nội</h3>
+	<h3>Địa Chỉ: Đại Từ,Đại Kim,Hoàng Mai,Hà Nội</h3>
+	<h3>Nghề Nghiệp: Lập Trình Viên aptech </h3>
+	<h3>Sở Thích: Ngủ và Đá Bóng</h3>
+		
+	</div>
+	</div>
+
+</body>
+</html>
